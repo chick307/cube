baba75dc4522bd32623c41f21a1f4307bfe1ece1
chick307 <chick307@gmail.com>
2020-01-01 07:23:31 +0900

    Display home directory entires

--- src/entries/main-window.tsx
+++ src/entries/main-window.tsx
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as path from 'path';
 
 import { remote } from 'electron';
@@ -7,10 +8,19 @@ import ReactDom from 'react-dom';
 const MainWindow = () => {
     const homeDirectoryPath = remote.app.getPath('home');
 
+    const homeDirectoryEntries = fs.readdirSync(homeDirectoryPath);
+
     return <>
         <div>
             {homeDirectoryPath}
         </div>
+        <ul>
+            {homeDirectoryEntries.map((name) => (
+                <li>
+                    {name}
+                </li>
+            ))}
+        </ul>
     </>;
 };
 
