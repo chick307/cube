0b040f3ef1fa65b89f7c3efb0d9ae6030cd83dbf
chick307 <chick307@gmail.com>
2020-01-01 07:12:20 +0900

    Display home directory path

--- src/entries/main-window.tsx
+++ src/entries/main-window.tsx
@@ -1,9 +1,16 @@
+import * as path from 'path';
+
+import { remote } from 'electron';
 import React from 'react';
 import ReactDom from 'react-dom';
 
 const MainWindow = () => {
+    const homeDirectoryPath = remote.app.getPath('home');
+
     return <>
-        Hello, World!
+        <div>
+            {homeDirectoryPath}
+        </div>
     </>;
 };
 
