45194f755e537d843eef3d08e8bc22a28a96c4c9
chick307 <chick307@gmail.com>
2020-01-01 07:50:55 +0900

    Add navigation feature

--- src/entries/main-window.tsx
+++ src/entries/main-window.tsx
@@ -5,22 +5,53 @@ import { remote } from 'electron';
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const MainWindow = () => {
-    const homeDirectoryPath = remote.app.getPath('home');
+const HOME_DIRECTORY_PATH = remote.app.getPath('home');
 
-    const homeDirectoryEntries = fs.readdirSync(homeDirectoryPath);
+type Entry = {
+    name: string;
+    type: 'file' | 'directory' | 'other';
+};
 
+const DirectoryEntryView = (props: { entry: Entry; onEntryClick: (entry: Entry) => void; }) => {
+    const { entry, onEntryClick } = props;
+    const onClick = React.useCallback(() => { onEntryClick(entry); }, [entry, onEntryClick]);
+    return <>
+        <span onDoubleClick={onClick}>
+            {entry.name}
+        </span>
+    </>;
+};
+
+const DirectoryView = (props: { directoryPath: string; onEntryClick: (entry: Entry) => void; }) => {
+    const { directoryPath, onEntryClick } = props;
+    const entries = fs.readdirSync(directoryPath).map((name): Entry => {
+        const stat = fs.statSync(path.join(directoryPath, name));
+        return { name, type: stat.isFile() ? 'file' : stat.isDirectory() ? 'directory' : 'other' };
+    });
     return <>
         <div>
-            {homeDirectoryPath}
+            {directoryPath}
         </div>
         <ul>
-            {homeDirectoryEntries.map((name) => (
-                <li>
-                    {name}
+            {entries.map((entry) => (
+                <li key={entry.name}>
+                    <DirectoryEntryView entry={entry} onEntryClick={onEntryClick} />
                 </li>
             ))}
         </ul>
+    </>;
+};
+
+const MainWindow = () => {
+    const [directoryPath, setDirectoryPath] = React.useState(HOME_DIRECTORY_PATH);
+
+    const onEntryClick = React.useCallback((entry: Entry) => {
+        if (entry.type === 'directory')
+            setDirectoryPath(path.resolve(directoryPath, entry.name));
+    }, [directoryPath]);
+
+    return <>
+        <DirectoryView directoryPath={directoryPath} onEntryClick={onEntryClick} />
     </>;
 };
 
