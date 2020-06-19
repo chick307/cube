4e78c01bbed2c609cca4b733c6ea3a3cca9ff895
chick307 <chick307@gmail.com>
2020-06-20 01:02:17 +0900

    Implement entry.getParentEntry() method

--- src/entities/directory-entry.ts
+++ src/entities/directory-entry.ts
@@ -11,8 +11,18 @@ export class DirectoryEntry extends Entry {
 declare module './entry' {
     interface Entry {
         isDirectory(): this is DirectoryEntry;
+
+        getParentEntry(): DirectoryEntry | null;
     }
 }
+
+Entry.prototype.getParentEntry = function getParentEntry(this: Entry): DirectoryEntry | null {
+    const parentPath = this.path.getParentPath();
+    if (parentPath === null)
+        return null;
+    const parentEntry = new DirectoryEntry(parentPath);
+    return parentEntry;
+};
 
 Entry.prototype.isDirectory = () => false;
 
