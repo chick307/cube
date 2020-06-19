748119f4176bf80c19eb953e9479f1a7a5f84f2d
chick307 <chick307@gmail.com>
2020-06-20 02:51:02 +0900

    Add useTask hook

---
+++ src/hooks/use-task.ts
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export class Canceled {
+    //
+}
+
+export type TaskContext = {
+    isCanceled: () => boolean;
+    wrapPromise: <T>(promise: Promise<T>) => Promise<T>;
+};
+
+export type TaskCallback<T> = (context: TaskContext) => Promise<T>;
+
+export const useTask = <T>(f: TaskCallback<T>, deps?: React.DependencyList) => {
+    const [result, setResult] = React.useState<[T] | [undefined, any] | []>(() => []);
+
+    React.useEffect(() => {
+        const token = Object.create(null);
+        let canceled = false;
+        let cancel: (() => any) | null = null;
+        let canceledPromise: Promise<any> = new Promise<any>((resolve) => {
+            cancel = () => {
+                resolve(token);
+            };
+        });
+
+        f({
+            isCanceled: () => canceled,
+            wrapPromise: async <T>(promise: Promise<T>): Promise<T> => {
+                const result = await Promise.race([promise, canceledPromise]);
+                if (canceled)
+                    throw new Canceled();
+                return result;
+            },
+        }).then((r) => {
+            setResult([r]);
+        }, (e) => {
+            setResult([undefined, e]);
+        });
+
+        return () => {
+            canceled = true;
+            if (cancel !== null)
+                cancel();
+        };
+    }, deps);
+
+    return result;
+};
