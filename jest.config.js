4cba571bcd33c3ac7eaaadaef5806ae09e3773c9
chick307 <chick307@gmail.com>
2021-05-06 17:58:50 +0900

    Add a script to collect the coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+    collectCoverage: false,
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.(js|ts|tsx)',
+    ],
+    coverageDirectory: '<rootDir>/coverage',
+    coveragePathIgnorePatterns: [],
+    coverageReporters: ['lcov'],
     preset: 'ts-jest/presets/js-with-ts',
     testMatch: ['<rootDir>/src/**/*.spec.(js|ts|tsx)'],
 };
