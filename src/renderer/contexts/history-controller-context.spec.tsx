61986af7ad52f1513c8f614f54a41153ae5dd265
chick307 <chick307@gmail.com>
2021-05-07 05:18:10 +0900

    Create HistoryController context

---
+++ src/renderer/contexts/history-controller-context.spec.tsx
@@ -0,0 +1,69 @@
+import ReactDom from 'react-dom';
+import TestUtils from 'react-dom/test-utils';
+
+import { HistoryController } from '../controllers/history-controller';
+import { HistoryControllerProvider, useHistoryController } from './history-controller-context';
+
+let container: HTMLElement;
+
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    ReactDom.unmountComponentAtNode(container);
+    container.remove();
+    container = null!;
+});
+
+describe('HistoryController context', () => {
+    describe('HistoryControllerProvider component', () => {
+        test('it provides a HistoryController instance', () => {
+            let instance: HistoryController | null = null;
+
+            const Component = () => {
+                const historyController = useHistoryController();
+                instance = historyController;
+                return <></>;
+            };
+
+            const historyControllerInstance: HistoryController = {
+                goBack: () => {},
+                navigate: () => {},
+            };
+
+            TestUtils.act(() => {
+                ReactDom.render((
+                    <HistoryControllerProvider value={historyControllerInstance}>
+                        <Component />
+                    </HistoryControllerProvider>
+                ), container);
+            });
+
+            expect(instance).toBe(historyControllerInstance);
+        });
+    });
+
+    describe('useHistoryController hook', () => {
+        test('it throws an error if HistoryController instance is not provided', () => {
+            const handleError = jest.fn();
+            const Component = () => {
+                try {
+                    useHistoryController();
+                } catch(e) {
+                    handleError(e);
+                }
+                return <></>;
+            };
+
+            TestUtils.act(() => {
+                ReactDom.render((
+                    <Component />
+                ), container);
+            });
+
+            expect(handleError).toHaveBeenCalledTimes(1);
+        });
+    });
+});
