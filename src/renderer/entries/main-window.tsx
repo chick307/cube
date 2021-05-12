cc8f413c29c7976cbb81dd72d0adf91062649ee4
chick307 <chick307@gmail.com>
2021-05-13 06:05:57 +0900

    Add EntryServiceProvider to MainWindow component

--- src/renderer/entries/main-window.tsx
+++ src/renderer/entries/main-window.tsx
@@ -2,25 +2,41 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import { LocalFileSystem } from '../../common/entities/local-file-system';
+import { createContainer, createFactory } from '../../common/utils/create-container';
 import { EntryView } from '../components/entry-view';
+import { EntryServiceProvider } from '../contexts/entry-service-context';
 import { HistoryControllerProvider } from '../contexts/history-controller-context';
 import { HistoryControllerImpl } from '../controllers/history-controller';
 import { HistoryStore } from '../stores/history-store';
-import { LocalFileSystemService } from '../services/local-file-system-service';
 import { composeElements } from '../utils/compose-elements';
 import styles from './main-window.css';
+import { EntryServiceImpl } from '../services/entry-service';
+import { LocalEntryService, LocalEntryServiceImpl } from '../services/local-entry-service';
+import { ZipEntryServiceImpl } from '../services/zip-entry-service';
 
 const MainWindow = () => {
-    const localFileSystemService = React.useMemo(() => new LocalFileSystemService(), []);
-    const historyStore = React.useMemo(() => new HistoryStore({
-        historyState: {
-            entry: localFileSystemService.getHomeDirectory(),
-            fileSystem: new LocalFileSystem(),
-        },
-    }), []);
-    const historyController = React.useMemo(() => new HistoryControllerImpl({ historyStore }), [historyStore]);
+    const {
+        entryService,
+        historyController,
+        historyStore,
+    } = React.useMemo(() => {
+        return createContainer({
+            entryService: EntryServiceImpl,
+            historyController: HistoryControllerImpl,
+            historyStore: HistoryStore,
+            historyState: createFactory(({ localEntryService }: {
+                localEntryService: LocalEntryService;
+            }) => ({
+                entry: localEntryService.getHomeDirectoryEntry(),
+                fileSystem: new LocalFileSystem(),
+            })),
+            localEntryService: LocalEntryServiceImpl,
+            zipEntryService: ZipEntryServiceImpl,
+        });
+    }, []);
 
     return composeElements([
+        <EntryServiceProvider value={entryService} />,
         <HistoryControllerProvider value={historyController} />,
         <EntryView className={styles.mainContent} mainContent={true} {...{ historyStore }} />,
     ]);
