f71a3996b233065e161f64797befe1a26be0beb4
chick307 <chick307@gmail.com>
2021-07-20 06:55:29 +0900

    Create MainWindowService type

--- src/main/entries/main.ts
+++ src/main/entries/main.ts
@@ -1,51 +1,22 @@
-import * as path from 'path';
-
-import { BrowserWindow, app } from 'electron';
+import { app } from 'electron';
 
 import { createContainer } from '../../common/utils/create-container';
 import '../handlers/icon-handler';
 import '../handlers/path-handler';
+import { MainWindowServiceImpl } from '../services/main-window-service';
 import { PersistenceServiceImpl } from '../services/persistence-service';
 import { RestoreWindowStateServiceImpl } from '../services/restore-window-state-service';
 
 const container = createContainer({
+    mainWindowService: MainWindowServiceImpl,
     persistenceService: PersistenceServiceImpl,
     restoreWindowStateService: RestoreWindowStateServiceImpl,
 });
 
-const { restoreWindowStateService } = container;
+const {
+    mainWindowService,
+} = container;
 
-let mainWindow: BrowserWindow | null = null;
-
-const createMainWindow = () => {
-    if (mainWindow !== null)
-        throw Error();
-
-    const window = mainWindow = new BrowserWindow({
-        frame: false,
-        show: false,
-        titleBarStyle: 'hidden',
-        trafficLightPosition: { x: 12, y: 20 },
-        webPreferences: {
-            contextIsolation: false,
-            nodeIntegration: true,
-        },
-        ...restoreWindowStateService.getWindowOptions(),
-    });
-
-    restoreWindowStateService.observeWindow(window);
-
-    window.loadURL(`file://${path.resolve(__dirname, '../views/main-window.html')}`);
-
-    window.on('closed', () => {
-        if (mainWindow === window)
-            mainWindow = null;
-    });
-
-    window.on('ready-to-show', () => {
-        window.show();
-    });
-};
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin')
@@ -53,13 +24,9 @@ app.on('window-all-closed', () => {
 });
 
 app.on('ready', () => {
-    createMainWindow();
+    mainWindowService.activate();
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createMainWindow();
-    } else {
-        mainWindow.show();
-    }
+    mainWindowService.activate();
 });
---
+++ src/main/services/main-window-service.ts
@@ -0,0 +1,67 @@
+import * as path from 'path';
+
+import { BrowserWindow } from 'electron';
+
+import type { RestoreWindowStateService } from './restore-window-state-service';
+
+const MAIN_WINDOW_URL = `file://${path.resolve(__dirname, '../views/main-window.html')}`;
+
+export type MainWindowService = {
+    activate(): void;
+};
+
+export class MainWindowServiceImpl implements MainWindowService {
+    private _restoreWindowStateService: RestoreWindowStateService;
+
+    private _window: BrowserWindow | null = null;
+
+    constructor(params: {
+        restoreWindowStateService: RestoreWindowStateService;
+    }) {
+        this._restoreWindowStateService = params.restoreWindowStateService;
+    }
+
+    private async _createWindow(): Promise<BrowserWindow> {
+        if (this._window !== null)
+            throw Error();
+
+        this._window = new BrowserWindow({
+            frame: false,
+            show: false,
+            titleBarStyle: 'hidden',
+            trafficLightPosition: { x: 12, y: 20 },
+            webPreferences: {
+                contextIsolation: false,
+                nodeIntegration: true,
+            },
+            ...this._restoreWindowStateService.getWindowOptions(),
+        });
+
+        this._restoreWindowStateService.observeWindow(this._window);
+
+        this._window.loadURL(MAIN_WINDOW_URL);
+
+        this._window.on('closed', () => {
+            this._window = null;
+        });
+
+        await new Promise<void>((resolve) => {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            this._window!.once('ready-to-show', () => {
+                if (this._window !== null)
+                    this._window.show();
+                resolve();
+            });
+        });
+
+        return this._window;
+    }
+
+    activate() {
+        if (this._window === null) {
+            this._createWindow();
+        } else {
+            this._window.show();
+        }
+    }
+}
