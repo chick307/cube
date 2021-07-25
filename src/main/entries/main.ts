a0a3e39658eaa10d6c9356d8a152f10eae58991a
chick307 <chick307@gmail.com>
2021-07-25 17:22:16 +0900

    Handle open-file event

--- src/main/entries/main.ts
+++ src/main/entries/main.ts
@@ -4,12 +4,14 @@ import { createContainer } from '../../common/utils/create-container';
 import '../handlers/icon-handler';
 import '../handlers/path-handler';
 import { ApplicationMenuServiceImpl } from '../services/application-menu-service';
+import { LocalFileSystemServiceImpl } from '../services/local-file-system-service';
 import { MainWindowServiceImpl } from '../services/main-window-service';
 import { PersistenceServiceImpl } from '../services/persistence-service';
 import { RestoreWindowStateServiceImpl } from '../services/restore-window-state-service';
 
 const container = createContainer({
     applicationMenuService: ApplicationMenuServiceImpl,
+    localFileSystemService: LocalFileSystemServiceImpl,
     mainWindowService: MainWindowServiceImpl,
     persistenceService: PersistenceServiceImpl,
     restoreWindowStateService: RestoreWindowStateServiceImpl,
@@ -17,10 +19,30 @@ const container = createContainer({
 
 const {
     applicationMenuService,
+    localFileSystemService,
     mainWindowService,
 } = container;
 
+let ready = false;
+let startEntryPath: string | null = null;
+
 applicationMenuService.initialize();
+
+const openEntry = async (pathString: string) => {
+    const entry = await localFileSystemService.getEntryFromPath(pathString);
+    const fileSystem = localFileSystemService.getFileSystem();
+    mainWindowService.navigate({ entry, fileSystem });
+};
+
+app.on('open-file', (event, path) => {
+    console.log(path);
+    event.preventDefault();
+    if (!ready) {
+        startEntryPath = path;
+    } else {
+        openEntry(path);
+    }
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin')
@@ -28,7 +50,13 @@ app.on('window-all-closed', () => {
 });
 
 app.on('ready', () => {
-    mainWindowService.activate();
+    ready = true;
+    if (startEntryPath !== null) {
+        openEntry(startEntryPath);
+        startEntryPath = null;
+    } else {
+        mainWindowService.activate();
+    }
 });
 
 app.on('activate', () => {
