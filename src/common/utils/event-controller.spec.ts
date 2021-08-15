32c7eefeebe6f5d5155e13ce197d6755fe741560
chick307 <chick307@gmail.com>
2021-08-15 23:52:33 +0900

    Create EventController class

---
+++ src/common/utils/event-controller.spec.ts
@@ -0,0 +1,50 @@
+import { EventController } from './event-controller';
+
+describe('EventController class', () => {
+    describe('eventController.emit() method', () => {
+        test('it calls added listeners', async () => {
+            const eventController = new EventController();
+            const listeners = Array.from(new Array(3), () => jest.fn());
+            for (const listener of listeners)
+                eventController.signal.addListener(listener);
+            const event = { a: 123 };
+            eventController.emit(event);
+            await Promise.resolve();
+            for (const listener of listeners) {
+                expect(listener).toHaveBeenCalledTimes(1);
+                expect(listener).toHaveBeenCalledWith(event);
+            }
+            for (const listener of listeners)
+                listener.mockClear();
+            eventController.emit(event);
+            await Promise.resolve();
+            for (const listener of listeners) {
+                expect(listener).toHaveBeenCalledTimes(1);
+                expect(listener).toHaveBeenCalledWith(event);
+            }
+        });
+    });
+
+    describe('eventController.signal.addListener() method', () => {
+        test('it returns an object to remove the listener', async () => {
+            const eventController = new EventController();
+            const listener = jest.fn();
+            const listening = eventController.signal.addListener(listener);
+            const event = { a: 123 };
+            eventController.emit(event);
+            await Promise.resolve();
+            expect(listener).toHaveBeenCalledTimes(1);
+            expect(listener).toHaveBeenCalledWith(event);
+            listener.mockClear();
+            listening.removeListener();
+            eventController.emit(event);
+            await Promise.resolve();
+            expect(listener).toHaveBeenCalledTimes(0);
+            listener.mockClear();
+            listening.removeListener();
+            eventController.emit(event);
+            await Promise.resolve();
+            expect(listener).toHaveBeenCalledTimes(0);
+        });
+    });
+});
---
+++ src/common/utils/event-controller.ts
@@ -0,0 +1,62 @@
+export type EventListener<Event> = (event: Event) => void;
+
+export type EventSignalLike<Event> = {
+    addListener(listener: EventListener<Event>): {
+        removeListener(): void;
+    };
+};
+
+export class EventSignal<Event> implements EventSignalLike<Event> {
+    constructor(private _signal: EventSignalLike<Event>) {}
+
+    addListener(listener: EventListener<Event>): {
+        removeListener(): void;
+    } {
+        return this._signal.addListener(listener);
+    }
+}
+
+class EventSignalInternal<Event> implements EventSignalLike<Event> {
+    idCounter = 0;
+
+    listeners = [] as {
+        id: number;
+        listener: EventListener<Event>;
+    }[];
+
+    addListener(listener: EventListener<Event>): {
+        removeListener(): void;
+    } {
+        const id = ++this.idCounter;
+        this.listeners.push({ id, listener });
+        return {
+            removeListener: () => {
+                const index = this.listeners.findIndex((item) => item.id === id);
+                if (index !== -1)
+                    this.listeners.splice(index, 1);
+            },
+        };
+    }
+
+    emit(event: Event) {
+        Promise.resolve().then(() => {
+            for (const listener of this.listeners.map(({ listener }) => listener))
+                listener(event);
+        });
+    }
+}
+
+export class EventController<Event> {
+    private _internal: EventSignalInternal<Event>;
+
+    readonly signal: EventSignal<Event>;
+
+    constructor() {
+        this._internal = new EventSignalInternal();
+        this.signal = new EventSignal(this._internal);
+    }
+
+    emit(event: Event): void {
+        this._internal.emit(event);
+    }
+}
