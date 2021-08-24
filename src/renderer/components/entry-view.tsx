3ec0f5cf7bca28afa70c412cc4a0024971a6f9aa
chick307 <chick307@gmail.com>
2021-08-25 06:35:22 +0900

    Add the section of the entry view name to the status bar

--- src/renderer/components/entry-view.tsx
+++ src/renderer/components/entry-view.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 
+import { Entry } from '../../common/entities';
 import { DirectoryEntry } from '../../common/entities/directory-entry';
-import type { FileEntry } from '../../common/entities/file-entry';
 import { ZipFileSystem } from '../../common/entities/zip-file-system';
 import { EntryPath } from '../../common/values/entry-path';
 import { useHistoryController } from '../contexts/history-controller-context';
 import { useStatusBar, useStatusBarGateway } from '../gateways/status-bar-gateway';
 import { useRestate } from '../hooks/use-restate';
 import styles from './entry-view.css';
-import { BinaryEntryView } from './entry-views/binary-entry-view';
-import { ComicEntryView, isComicEntry } from './entry-views/comic-entry-view';
-import { CssEntryView, isCssEntry } from './entry-views/css-entry-view';
-import { DirectoryEntryView } from './entry-views/directory-entry-view';
-import { ImageEntryView, isImageEntry } from './entry-views/image-entry-view';
-import { JavaScriptEntryView, isJavaScriptEntry } from './entry-views/javascript-entry-view';
-import { MarkdownEntryView, isMarkdownEntry } from './entry-views/markdown-entry-view';
-import { MediaEntryView, isMediaEntry } from './entry-views/media-entry-view';
-import { PdfEntryView, isPdfEntry } from './entry-views/pdf-entry-view';
-import { SymbolicLinkEntryView } from './entry-views/symbolic-link-entry-view';
-import { TextEntryView, isTextEntry } from './entry-views/text-entry-view';
+import { entryViews } from './entry-views';
 import { GoBackButton } from './go-back-button';
 import { GoForwardButton } from './go-forward-button';
 
@@ -26,8 +16,7 @@ export type Props = {
     className?: string;
 };
 
-const isZipEntry = (entry: FileEntry) =>
-    /^\.(?:zip)$/.test(entry.path.getExtension());
+const isZipEntry = (entry: Entry) => entry.isFile() && /^\.(?:zip)$/.test(entry.path.getExtension());
 
 export const EntryView = (props: Props) => {
     const { className = '' } = props;
@@ -36,40 +25,18 @@ export const EntryView = (props: Props) => {
 
     const { current: { entry, fileSystem } } = useRestate(historyController.state);
 
-    const view = React.useMemo(() => {
-        const viewProps = { className: styles.view, fileSystem };
-
-        if (entry.isDirectory())
-            return <DirectoryEntryView {...{ entry, ...viewProps }} />;
-
-        if (entry.isSymbolicLink())
-            return <SymbolicLinkEntryView {...{ entry, ...viewProps }} />;
-
-        if (entry.isFile()) {
-            const fileEntryViewProps = { entry, ...viewProps };
-            if (isComicEntry(entry))
-                return <ComicEntryView {...fileEntryViewProps} />;
-            if (isCssEntry(entry))
-                return <CssEntryView {...fileEntryViewProps} />;
-            if (isImageEntry(entry))
-                return <ImageEntryView {...fileEntryViewProps} />;
-            if (isJavaScriptEntry(entry))
-                return <JavaScriptEntryView {...fileEntryViewProps} />;
-            if (isMarkdownEntry(entry))
-                return <MarkdownEntryView {...fileEntryViewProps} />;
-            if (isMediaEntry(entry))
-                return <MediaEntryView {...fileEntryViewProps} />;
-            if (isPdfEntry(entry))
-                return <PdfEntryView {...fileEntryViewProps} />;
-            if (isTextEntry(entry))
-                return <TextEntryView {...fileEntryViewProps} />;
-            if (isZipEntry(entry))
-                return null;
-            return <BinaryEntryView {...fileEntryViewProps} />;
-        }
-
-        return null;
-    }, [entry, fileSystem]);
+    const { entryView, view } = React.useMemo(() => {
+        if (isZipEntry(entry))
+            return null;
+        const entryView = entryViews.find((entryView) => entryView.test(entry));
+        if (entryView == null)
+            return null;
+        const view = entryView.render({ entry, fileSystem });
+        return { entryView, view };
+    }, [entry, fileSystem]) ?? {
+        entryView: null,
+        view: null,
+    };
 
     React.useEffect(() => {
         if (entry.isFile() && isZipEntry(entry)) {
@@ -101,6 +68,11 @@ export const EntryView = (props: Props) => {
             <StatusBarGateway>
                 <div className={styles.entryViewStatusBar}>
                     <StatusBarExit />
+                </div>
+                <div className={styles.entryViewNameContainer}>
+                    <span className={styles.entryViewName}>
+                        {entryView?.name ?? ''}
+                    </span>
                 </div>
             </StatusBarGateway>
         </div>
---
+++ src/renderer/components/entry-views/index.tsx
@@ -0,0 +1,123 @@
+import { DirectoryEntry, Entry, FileEntry, FileSystem, SymbolicLinkEntry } from '../../../common/entities';
+import { BinaryEntryView } from './binary-entry-view';
+import { ComicEntryView, isComicEntry } from './comic-entry-view';
+import { CssEntryView, isCssEntry } from './css-entry-view';
+import { DirectoryEntryView } from './directory-entry-view';
+import { ImageEntryView, isImageEntry } from './image-entry-view';
+import { JavaScriptEntryView, isJavaScriptEntry } from './javascript-entry-view';
+import { MarkdownEntryView, isMarkdownEntry } from './markdown-entry-view';
+import { MediaEntryView, isMediaEntry } from './media-entry-view';
+import { PdfEntryView, isPdfEntry } from './pdf-entry-view';
+import { SymbolicLinkEntryView } from './symbolic-link-entry-view';
+import { TextEntryView, isTextEntry } from './text-entry-view';
+
+/* eslint-disable react/display-name */
+export const entryViews = [
+    ...([
+        {
+            id: 'directory-entry-view',
+            name: 'Directory',
+            test: () => true,
+            render: (props) => (
+                <DirectoryEntryView {...props} />
+            ),
+        },
+    ] as {
+        id: string;
+        name: string;
+        test: (entry: DirectoryEntry) => boolean;
+        render: (props: { entry: DirectoryEntry; fileSystem: FileSystem; }) => React.ReactNode;
+    }[]).map((entryView) => ({
+        ...entryView,
+        test: (entry: Entry) => entry.isDirectory() && entryView.test(entry),
+        render: (props: { entry: Entry; fileSystem: FileSystem; }) =>
+            entryView.render({ ...props, entry: props.entry as DirectoryEntry }),
+    })),
+    ...([
+        {
+            id: 'symbolic-link-view',
+            name: 'Symbolic Link',
+            test: (entry) => entry.isSymbolicLink(),
+            render: (props) => <SymbolicLinkEntryView {...props} />,
+        },
+    ] as {
+        id: string;
+        name: string;
+        test: (entry: SymbolicLinkEntry) => boolean;
+        render: (props: { entry: SymbolicLinkEntry; fileSystem: FileSystem; }) => React.ReactNode;
+    }[]).map((entryView) => ({
+        ...entryView,
+        test: (entry: Entry) => entry.isSymbolicLink() && entryView.test(entry),
+        render: (props: {
+            entry: Entry;
+            fileSystem: FileSystem;
+        }) => entryView.render({ ...props, entry: props.entry as SymbolicLinkEntry }),
+    })),
+    ...([
+        {
+            id: 'comic-entry-view',
+            name: 'Comic',
+            test: (entry) => isComicEntry(entry),
+            render: (props) => <ComicEntryView {...props} />,
+        },
+        {
+            id: 'css-entry-view',
+            name: 'CSS',
+            test: (entry) => isCssEntry(entry),
+            render: (props) => <CssEntryView {...props} />,
+        },
+        {
+            id: 'image-entry-view',
+            name: 'Image',
+            test: (entry) => isImageEntry(entry),
+            render: (props) => <ImageEntryView {...props} />,
+        },
+        {
+            id: 'javascript-entry-view',
+            name: 'JavaScript',
+            test: (entry) => isJavaScriptEntry(entry),
+            render: (props) => <JavaScriptEntryView {...props} />,
+        },
+        {
+            id: 'markdown-entry-view',
+            name: 'Markdown',
+            test: (entry) => isMarkdownEntry(entry),
+            render: (props) => <MarkdownEntryView {...props} />,
+        },
+        {
+            id: 'media-entry-view',
+            name: 'Media',
+            test: (entry) => isMediaEntry(entry),
+            render: (props) => <MediaEntryView {...props} />,
+        },
+        {
+            id: 'pdf-entry-view',
+            name: 'PDF',
+            test: (entry) => isPdfEntry(entry),
+            render: (props) => <PdfEntryView {...props} />,
+        },
+        {
+            id: 'text-entry-view',
+            name: 'Text',
+            test: (entry) => isTextEntry(entry),
+            render: (props) => <TextEntryView {...props} />,
+        },
+        {
+            id: 'binary-entry-view',
+            name: 'Binary',
+            test: (entry: Entry) => entry.isFile(),
+            render: (props) => <BinaryEntryView {...props} />,
+        },
+    ] as {
+        id: string;
+        name: string;
+        test: (entry: FileEntry) => boolean;
+        render: (props: { entry: FileEntry; fileSystem: FileSystem; }) => React.ReactNode;
+    }[]).map((fileEntryView) => ({
+        ...fileEntryView,
+        test: (entry: Entry) => entry.isFile() && fileEntryView.test(entry),
+        render: (props: { entry: Entry; fileSystem: FileSystem; }) =>
+            fileEntryView.render({ ...props, entry: props.entry as FileEntry }),
+    })),
+];
+/* eslint-enable react/display-name */
