1eb82d6a42770c0b4ab85d16075a55cfe1e0bbee
chick307 <chick307@gmail.com>
2021-09-02 20:49:47 +0900

    Add context menu to TabView component

--- src/renderer/components/tab-view.tsx
+++ src/renderer/components/tab-view.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { HistoryControllerProvider } from '../contexts/history-controller-context';
 import { useTabController } from '../contexts/tab-controller-context';
+import type { TabState } from '../controllers/tab-controller';
 import { useStatusBar } from '../gateways/status-bar-gateway';
 import { useRestate } from '../hooks/use-restate';
 import { composeElements } from '../utils/compose-elements';
@@ -10,9 +11,48 @@ import { EntryView } from './entry-view';
 import { TabAddButton } from './tab/tab-add-button';
 import { TabCloseButton } from './tab/tab-close-button';
 import styles from './tab-view.css';
+import { TabViewContextMenu } from './tab/tab-view-context-menu';
+import { TabContextMenu } from './tab/tab-context-menu';
 
 export type Props = {
     className?: string;
+};
+
+const Tab = (props: {
+    tab: TabState;
+}) => {
+    const { tab } = props;
+
+    const tabController = useTabController();
+
+    const className = `${styles.tab} ${tab.active ? styles.active : ''}`;
+
+    const onAuxClick = React.useCallback(() => {
+        tabController.removeTab({ id: tab.id });
+    }, [tabController]);
+
+    const onClick = React.useCallback(() => {
+        tabController.selectTab({ id: tab.id });
+    }, [tabController]);
+
+    const { title } = tab;
+
+    return composeElements(
+        <TabContextMenu tabId={tab.id} />,
+        <div key={tab.id} {...{ className, onAuxClick, onClick, title }}>
+            <span className={styles.tabMargin} />
+            <span className={styles.tabIcon}>
+                <EntryIcon entry={tab.historyController.state.current.current.entry} />
+            </span>
+            <span className={styles.tabMargin} />
+            <span className={styles.tabTitle}>
+                {tab.title}
+            </span>
+            <span className={styles.tabMargin} />
+            <TabCloseButton className={styles.closeButton} tabId={tab.id} />
+            <span className={styles.tabMargin} />
+        </div>,
+    );
 };
 
 export const TabView = (props: Props) => {
@@ -22,32 +62,7 @@ export const TabView = (props: Props) => {
 
     const { tabs } = useRestate(tabController.state);
 
-    const onTabClick = React.useCallback((event: React.MouseEvent) => {
-        const tab = (event.target as HTMLElement).closest<HTMLElement>('[data-tab-id]');
-        const id = parseInt(tab?.dataset.tabId ?? '0', 10);
-        if (!id)
-            return;
-        tabController.selectTab({ id });
-    }, [tabController]);
-
-    const tabElements = tabs.map((tab) => {
-        return (
-            <div key={tab.id} className={`${styles.tab} ${tab.active ? styles.active : ''}`}
-                data-tab-id={tab.id} onClick={onTabClick} title={tab.title}>
-                <span className={styles.tabMargin} />
-                <span className={styles.tabIcon}>
-                    <EntryIcon entry={tab.historyController.state.current.current.entry} />
-                </span>
-                <span className={styles.tabMargin} />
-                <span className={styles.tabTitle}>
-                    {tab.title}
-                </span>
-                <span className={styles.tabMargin} />
-                <TabCloseButton className={styles.closeButton} tabId={tab.id} />
-                <span className={styles.tabMargin} />
-            </div>
-        );
-    });
+    const tabElements = tabs.map((tab) => <Tab key={tab.id} {...{ tab }} />);
 
     const contents = tabs.map((tab) => {
         return composeElements(
@@ -60,10 +75,12 @@ export const TabView = (props: Props) => {
     return composeElements(
         <StatusBarProvider />,
         <div className={`${styles.view} ${props.className ?? ''}`}>
-            <div className={styles.tabs}>
-                {tabElements}
-                <TabAddButton />
-            </div>
+            <TabViewContextMenu>
+                <div className={styles.tabs}>
+                    {tabElements}
+                    <TabAddButton />
+                </div>
+            </TabViewContextMenu>
             <div className={styles.contents}>{contents}</div>
             <div className={styles.statusBar}>
                 <StatusBarExit />
