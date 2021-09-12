9f1117f8b3adb3d85dad3d23f0fd6fc23b1a9f76
chick307 <chick307@gmail.com>
2021-09-12 18:57:41 +0900

    Fix flaky test

--- src/renderer/services/main-channel-service.spec.ts
+++ src/renderer/services/main-channel-service.spec.ts
@@ -1,11 +1,20 @@
 import { MessageChannel } from 'worker_threads';
 
-import { timeout } from '../../common/utils/immediate';
 import { MainChannelServiceImpl } from './main-channel-service';
 
 const createMessagePorts = (): [MessagePort, MessagePort] => {
     const { port1, port2 } = new MessageChannel() as any;
     return [port1, port2];
+};
+
+const waitMessage = async () => {
+    const channel = new MessageChannel();
+    await new Promise<void>((resolve) => {
+        channel.port1.on('message', () => resolve());
+        channel.port1.start();
+        channel.port2.postMessage(0);
+    });
+    channel.port1.close();
 };
 
 describe('MainChannelService type', () => {
@@ -17,7 +26,7 @@ describe('MainChannelService type', () => {
                 const listener = jest.fn();
                 mainChannelService.onMessage.addListener(listener);
                 port2.postMessage({ type: 'window.add-tab' });
-                await timeout();
+                await waitMessage();
                 expect(listener).toHaveBeenCalledTimes(1);
                 expect(listener).toHaveBeenCalledWith({ type: 'window.add-tab' });
                 listener.mockClear();
@@ -36,7 +45,7 @@ describe('MainChannelService type', () => {
                 port2.addEventListener('message', messageHandler);
                 port2.start();
                 mainChannelService.postMessage({ type: 'window.ready-to-show' });
-                await timeout();
+                await waitMessage();
                 expect(messageHandler).toHaveBeenCalledTimes(1);
                 expect(messageHandler.mock.calls[0][0].data).toEqual({ type: 'window.ready-to-show' });
                 messageHandler.mockClear();
