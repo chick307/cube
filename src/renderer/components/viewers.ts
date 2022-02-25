2b36b3decb544741e41a688d9851db92c98ac3a7
chick307 <chick307@gmail.com>
2022-02-25 21:33:36 +0900

    Create `SymbolicLinkViewer` component

--- src/renderer/components/viewers.ts
+++ src/renderer/components/viewers.ts
@@ -1,1 +1,2 @@
 export { DirectoryViewer, Props as DirectoryViewerProps } from './viewers/directory-viewer';
+export { SymbolicLinkViewer, Props as SymbolicLinkViewerProps } from './viewers/symbolic-link-viewer';
