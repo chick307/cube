ddfe331dfc9b8ed2238b476d18a42eefa0015ce3
chick307 <chick307@gmail.com>
2022-03-27 05:14:37 +0900

    Delete `PdfEntryView` component

--- src/renderer/components/entry-views.tsx
+++ src/renderer/components/entry-views.tsx
@@ -2,5 +2,4 @@ export { BinaryEntryView } from './entry-views/binary-entry-view';
 export { CssEntryView } from './entry-views/css-entry-view';
 export { JavaScriptEntryView } from './entry-views/javascript-entry-view';
 export { MediaEntryView } from './entry-views/media-entry-view';
-export { PdfEntryView } from './entry-views/pdf-entry-view';
 export { TextEntryView } from './entry-views/text-entry-view';
