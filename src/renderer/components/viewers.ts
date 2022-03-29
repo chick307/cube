7780f6d00128bbf84123b26cbae71eeea13d483c
chick307 <chick307@gmail.com>
2022-03-30 08:08:42 +0900

    Create `MediaViewer` component

--- src/renderer/components/viewers.ts
+++ src/renderer/components/viewers.ts
@@ -2,6 +2,7 @@ export { ComicViewer, Props as ComicViewerProps } from './viewers/comic-viewer';
 export { DirectoryViewer, Props as DirectoryViewerProps } from './viewers/directory-viewer';
 export { ImageViewer, Props as ImageViewerProps } from './viewers/image-viewer';
 export { MarkdownViewer, Props as MarkdownViewerProps } from './viewers/markdown-viewer';
+export { MediaViewer, Props as MediaViewerProps } from './viewers/media-viewer';
 export { PdfViewer, Props as PdfViewerProps } from './viewers/pdf-viewer';
 export { SymbolicLinkViewer, Props as SymbolicLinkViewerProps } from './viewers/symbolic-link-viewer';
 export { TsvViewer, Props as TsvViewerProps } from './viewers/tsv-viewer';
