9fefaadcbaea0a105ff20dc9d29c05dadbd873f1
chick307 <chick307@gmail.com>
2022-03-31 05:10:09 +0900

    Delete `TextEntryView` component

--- src/renderer/components/entry-views.tsx
+++ src/renderer/components/entry-views.tsx
@@ -1,4 +1,3 @@
 export { BinaryEntryView } from './entry-views/binary-entry-view';
 export { CssEntryView } from './entry-views/css-entry-view';
 export { JavaScriptEntryView } from './entry-views/javascript-entry-view';
-export { TextEntryView } from './entry-views/text-entry-view';
