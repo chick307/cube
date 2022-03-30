73caaf10fe5a7d0c266296c075eb88b3a77878c0
chick307 <chick307@gmail.com>
2022-03-30 09:28:03 +0900

    Create `textViewerState.scrollPosition` property

--- src/common/values/viewer-state/text-viewer-state.spec.ts
+++ src/common/values/viewer-state/text-viewer-state.spec.ts
@@ -1,9 +1,14 @@
+import { Point } from '../point';
 import { TextViewerState } from '../viewer-state';
+
+const defaultJson = { type: 'text', scrollPosition: { x: 0, y: 0 } };
 
 describe('TextViewerState class', () => {
     describe('TextViewerState.fromJson() method', () => {
         test('it returns an instance of TextViewerState class', () => {
             expect(TextViewerState.fromJson({ type: 'text' })).toEqual(new TextViewerState());
+            expect(TextViewerState.fromJson({ type: 'text', scrollPosition: { x: 10, y: 20 } }))
+                .toEqual(new TextViewerState({ scrollPosition: new Point(10, 20) }));
         });
 
         test('it throws an error if the passed JSON is invalid', () => {
@@ -14,9 +19,20 @@ describe('TextViewerState class', () => {
         });
     });
 
+    describe('textViewerState.setScrollPosition() method', () => {
+        test('it creates a new state', () => {
+            expect(new TextViewerState().setScrollPosition(new Point(3, 14)))
+                .toEqual(new TextViewerState({ scrollPosition: new Point(3, 14) }));
+            expect(new TextViewerState({ scrollPosition: new Point(1, 12) }).setScrollPosition(new Point(3, 58)))
+                .toEqual(new TextViewerState({ scrollPosition: new Point(3, 58) }));
+        });
+    });
+
     describe('textViewerState.toJson() method', () => {
         test('it returns JSON object', () => {
-            expect(new TextViewerState().toJson()).toEqual({ type: 'text' });
+            expect(new TextViewerState().toJson()).toEqual({ ...defaultJson });
+            expect(new TextViewerState({ scrollPosition: new Point(10, 20) }).toJson())
+                .toEqual({ ...defaultJson, scrollPosition: { x: 10, y: 20 } });
         });
     });
 });
--- src/common/values/viewer-state/text-viewer-state.ts
+++ src/common/values/viewer-state/text-viewer-state.ts
@@ -1,11 +1,23 @@
+import { Point, PointJson } from '../point';
 import { ViewerState, viewerStateTypes } from './viewer-state';
 
 export type TextViewerStateJson = {
     type: 'text';
+
+    scrollPosition?: PointJson | null | undefined;
 };
 
 export class TextViewerState extends ViewerState {
+    readonly scrollPosition: Point;
+
     readonly type = 'text';
+
+    constructor(params?: {
+        readonly scrollPosition?: Point | null | undefined;
+    }) {
+        super();
+        this.scrollPosition = params?.scrollPosition ?? new Point(0, 0);
+    }
 
     static fromJson(json: TextViewerStateJson): TextViewerState;
 
@@ -15,13 +27,21 @@ export class TextViewerState extends ViewerState {
     static fromJson(json: any): TextViewerState {
         if (json == null || json.type !== 'text')
             throw Error();
-        const textViewerState = new TextViewerState();
+        const scrollPosition = json.scrollPosition == null ? null : Point.fromJson(json.scrollPosition);
+        const textViewerState = new TextViewerState({ scrollPosition });
         return textViewerState;
+    }
+
+    setScrollPosition(position: Point): TextViewerState {
+        return new TextViewerState({
+            scrollPosition: position,
+        });
     }
 
     override toJson(): TextViewerStateJson {
         return {
             type: 'text',
+            scrollPosition: this.scrollPosition.toJson(),
         };
     }
 }
