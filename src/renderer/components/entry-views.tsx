56085eb413672cfd00256bb7bf05f10a2c994b3e
chick307 <chick307@gmail.com>
2022-03-31 11:59:29 +0900

    Delete `CssEntryView` component

--- src/renderer/components/entry-views.tsx
+++ src/renderer/components/entry-views.tsx
@@ -1,3 +1,2 @@
 export { BinaryEntryView } from './entry-views/binary-entry-view';
-export { CssEntryView } from './entry-views/css-entry-view';
 export { JavaScriptEntryView } from './entry-views/javascript-entry-view';
