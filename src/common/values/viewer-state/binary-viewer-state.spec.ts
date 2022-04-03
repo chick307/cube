e00f15b5b7d09e2b484914f86ac03f262891108e
chick307 <chick307@gmail.com>
2022-04-03 17:22:54 +0900

    Create `bianryViewerState.scrollPosition` property

--- src/common/values/viewer-state/binary-viewer-state.spec.ts
+++ src/common/values/viewer-state/binary-viewer-state.spec.ts
@@ -1,9 +1,23 @@
+import { Point } from '../point';
 import { BinaryViewerState } from '../viewer-state';
+
+const defaultJson = {
+    type: 'binary',
+    scrollPosition: { x: 0, y: 0 },
+};
+
+const scrollPositions = [new Point(0, 0), new Point(10, 20), new Point(33, 44)];
+
+const combinations = [{}]
+    .flatMap((values) => scrollPositions.map((v) => ({ ...values, scrollPosition: v })))
+    .concat([]);
 
 describe('BinaryViewerState class', () => {
     describe('BinaryViewerState.fromJson() method', () => {
         test('it returns an instance of BinaryViewerState class', () => {
             expect(BinaryViewerState.fromJson({ type: 'binary' })).toEqual(new BinaryViewerState());
+            expect(BinaryViewerState.fromJson({ type: 'binary', scrollPosition: { x: 10, y: 20 } }))
+                .toEqual(new BinaryViewerState({ scrollPosition: new Point(10, 20) }));
         });
 
         test('it throws an error if the passed JSON is invalid', () => {
@@ -14,9 +28,40 @@ describe('BinaryViewerState class', () => {
         });
     });
 
+    describe('binaryViewerState.equals() method', () => {
+        test('it returns whether the passed object has the same values', () => {
+            for (let i = 0; i < combinations.length; i++) {
+                const a = new BinaryViewerState(combinations[i]);
+                expect(a.equals(null)).toBe(false);
+                expect(a.equals(undefined)).toBe(false);
+                expect(a.equals(a)).toBe(true);
+                for (let j = 0; j < combinations.length; j++) {
+                    const b = new BinaryViewerState(combinations[j]);
+                    if (i === j) {
+                        expect(a.equals(b)).toBe(true);
+                    } else {
+                        expect(a.equals(b)).toBe(false);
+                    }
+                }
+            }
+        });
+    });
+
+    describe('textViewerState.setScrollPosition() method', () => {
+        test('it creates a new state', () => {
+            for (let i = 0; i < combinations.length; i++) {
+                const a = new BinaryViewerState(combinations[i]);
+                for (let j = 0; j < scrollPositions.length; j++) {
+                    expect(a.setScrollPosition(scrollPositions[j]))
+                        .toEqual(new BinaryViewerState({ ...combinations[i], scrollPosition: scrollPositions[j] }));
+                }
+            }
+        });
+    });
+
     describe('binaryViewerState.toJson() method', () => {
         test('it returns JSON object', () => {
-            expect(new BinaryViewerState().toJson()).toEqual({ type: 'binary' });
+            expect(new BinaryViewerState().toJson()).toEqual({ ...defaultJson });
         });
     });
 });
